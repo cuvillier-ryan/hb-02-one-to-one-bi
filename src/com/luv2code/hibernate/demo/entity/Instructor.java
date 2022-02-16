93dd8f2c22d8849b90db2ecb1632cee263060db3
cuvillier.ryan@gmail.com <cuvillier.ryan@gmail.com>
2022-02-16 07:20:39 -0600

    added OneToMany relationship Instructor to Course

--- src/com/luv2code/hibernate/demo/entity/Instructor.java
+++ src/com/luv2code/hibernate/demo/entity/Instructor.java
@@ -1,5 +1,8 @@
 package com.luv2code.hibernate.demo.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -7,48 +10,52 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="instructor")
+@Table(name = "instructor")
 public class Instructor {
 
-	
 	// annotate class as an entity and map to db table
-	
+
 	// define the fields
-	
+
 	// annotate the fields with db column names
-	
+
 	// ** set up mapping to instructorDetail entity **
-	
+
 	// create constructors
-	
+
 	// generate getters/setters methods
-	
-	// generate toString() method	
-	
+
+	// generate toString() method
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name="id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private int id;
-	
-	@Column(name="first_name")
+
+	@Column(name = "first_name")
 	private String firstName;
-	
-	@Column(name="last_name")
+
+	@Column(name = "last_name")
 	private String lastName;
-	
-	@Column(name="email")
+
+	@Column(name = "email")
 	private String email;
-	
-	@OneToOne(cascade=CascadeType.ALL)
-	@JoinColumn(name="instructor_detail_id")
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "instructor_detail_id")
 	private InstructorDetail instructorDetail;
-	
+
+	@OneToMany(mappedBy = "instructor", cascade = { CascadeType.PERSIST, CascadeType.DETACH, CascadeType.MERGE,
+			CascadeType.REFRESH })
+	private List<Course> courses;
+
 	public Instructor() {
-		
+
 	}
 
 	public Instructor(String firstName, String lastName, String email) {
@@ -97,12 +104,31 @@ public class Instructor {
 		this.instructorDetail = instructorDetail;
 	}
 
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+
+	// add convenience methods for bi-directional relationship
+
+	public void add(Course tempCourse) {
+
+		if (courses == null) {
+			courses = new ArrayList<>();
+		}
+		
+		courses.add(tempCourse);
+		
+		tempCourse.setInstructor(this);
+	}
+
 	@Override
 	public String toString() {
 		return "Instructor [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
 				+ ", instructorDetail=" + instructorDetail + "]";
 	}
-	
-	
-	
+
 }
